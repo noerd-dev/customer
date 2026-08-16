cd5d336a1bbd28599b4b04cbdd1eafc43e4efe9d
Johannes Brock <info@johannes-brock.de>
2026-08-16 23:30:00 +0200

    Use the noerd auth middleware group

--- routes/customer-routes.php
+++ routes/customer-routes.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => ['auth', 'verified', 'web']], function (): void {
+Route::group(['middleware' => ['noerd']], function (): void {
     Route::livewire('customers', 'customer::customers-list')->name('customers');
     Route::livewire('customer/{modelId}', 'customer::customer-detail')->name('customer.detail');
 });
